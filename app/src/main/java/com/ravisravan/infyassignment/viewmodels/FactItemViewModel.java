38f5696d18183dedc5570d47f361690c01177ceb
Ravi Sravan Kumar <ravikumar@blackberry.com>
2018-07-13 15:46:39 +0530

    Loaded images using Glide. Display in Recycler view is done. Avoid call when rotated and use of RXJava is pending.

--- app/src/main/java/com/ravisravan/infyassignment/viewmodels/FactItemViewModel.java
+++ app/src/main/java/com/ravisravan/infyassignment/viewmodels/FactItemViewModel.java
@@ -1,7 +1,14 @@
 package com.ravisravan.infyassignment.viewmodels;
 
 import android.databinding.BaseObservable;
+import android.databinding.BindingAdapter;
+import android.databinding.ObservableField;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.ravisravan.infyassignment.GlideApp;
+import com.ravisravan.infyassignment.R;
 import com.ravisravan.infyassignment.models.Row;
 
 /**
@@ -11,13 +18,30 @@ import com.ravisravan.infyassignment.models.Row;
 public class FactItemViewModel extends BaseObservable {
 
     public Row row;
+    public String imageUrl;
 
     public FactItemViewModel(Row row) {
         this.row = row;
+        this.imageUrl = row.getImageHref();
+    }
+
+    public String getImageUrl() {
+        // The URL will usually come from a model (i.e Row)
+        return imageUrl;
     }
 
     public void setRow(Row row){
         this.row = row;
+        this.imageUrl = row.getImageHref();
         notifyChange();
+    }
+
+    @BindingAdapter("imageUrl")
+    public static void loadImage(ImageView view, String imageUrl) {
+        GlideApp.with(view.getContext())
+                .load(imageUrl)
+                .placeholder(R.drawable.ic_launcher_background)
+                .fitCenter()
+                .into(view);
     }
 }
